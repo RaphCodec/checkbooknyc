d1adf8d458129c1681809c16a839942714ce4001
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2025-06-01 19:40:03 -0400

    Added Revenue API class

--- checkbooknyc/__init__.py
+++ checkbooknyc/__init__.py
@@ -1,3 +1,4 @@
 from .client import CheckbookNYCClient as CheckbookNYCClient
 from .client import Contracts as Contracts
 from .client import Budget as Budget
+from .client import Revenue as Revenue
